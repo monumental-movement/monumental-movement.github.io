c86ef046f36d96ee05c62f703d254dd4d68fbae2
monumental-movement <monumental.movement.records@gmail.com>
2024-04-08 22:55:01 +0900

    fixed

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -158,7 +158,7 @@ $(document).ready(function(){
 
     //Click event to scroll to top
     $('.scroll-top').click(function(){
-        $('html, body').animate({scrollTop : 0},300);
+        $('html, body').animate({scrollTop : 0},1);
         return false;
     });
 
